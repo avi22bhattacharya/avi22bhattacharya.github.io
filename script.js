79909d07f228eb2b436c568f8fe744a6ce7738fd
Avi Bhattacharya <61898077+avi22bhattacharya@users.noreply.github.com>
2024-06-18 12:42:49 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,4 +7,28 @@ document.addEventListener('scroll', function() {
       workExperienceDiv.classList.add('reveal');
     }
   });
-  
+
+//   $(function () {
+//     $('.genealogy-tree ul').hide();
+//     $('.genealogy-tree>ul').show();
+//     $('.genealogy-tree ul.active').show();
+//     $('.genealogy-tree li').on('click', function (e) {
+//         var children = $(this).find('> ul');
+//         if (children.is(":visible")) children.hide('fast').removeClass('active');
+//         else children.show('fast').addClass('active');
+//         e.stopPropagation();
+//     });
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const memberViewBoxes = document.querySelectorAll('.member-view-box');
+
+  memberViewBoxes.forEach(memberViewBox => {
+    memberViewBox.addEventListener('click', function(event) {
+      // Prevent the event from bubbling up to parent .member-view-box elements
+      event.stopPropagation();
+      memberViewBox.classList.toggle('expanded');
+    });
+  });
+});
