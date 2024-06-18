611754420074c8306499e03c4b56c612f7b80abc
Avi Bhattacharya <61898077+avi22bhattacharya@users.noreply.github.com>
2024-06-18 18:26:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-document.addEventListener('scroll', function() {
-    const workExperienceDiv = document.getElementById('work-experience');
-    const scrollPosition = window.scrollY + window.innerHeight;
-    const elementPosition = workExperienceDiv.offsetTop;
-  
-    if (scrollPosition > elementPosition) {
-      workExperienceDiv.classList.add('reveal');
-    }
-  });
+var slideIndex = 0;
+var slides = document.querySelectorAll('#slideshow h1');
 
-//   $(function () {
-//     $('.genealogy-tree ul').hide();
-//     $('.genealogy-tree>ul').show();
-//     $('.genealogy-tree ul.active').show();
-//     $('.genealogy-tree li').on('click', function (e) {
-//         var children = $(this).find('> ul');
-//         if (children.is(":visible")) children.hide('fast').removeClass('active');
-//         else children.show('fast').addClass('active');
-//         e.stopPropagation();
-//     });
-// });
+function showSlides() {
+slideIndex++;
+if (slideIndex >= slides.length) {
+slideIndex = 0;
+}
 
+for (var i = 0; i < slides.length; i++) {
+slides[i].classList.remove('active');
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-  const memberViewBoxes = document.querySelectorAll('.member-view-box');
+slides[slideIndex].classList.add('active');
+}
 
-  memberViewBoxes.forEach(memberViewBox => {
-    memberViewBox.addEventListener('click', function(event) {
-      // Prevent the event from bubbling up to parent .member-view-box elements
-      event.stopPropagation();
-      memberViewBox.classList.toggle('expanded');
-    });
-  });
-});
+setInterval(showSlides, 2000);
